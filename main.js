1b507c23f5b4e91d9a8607cb5a68721f04e06490
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2024-08-20 15:58:02 +0530

    js modified

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let localStream; // Local stream object
 let remoteStream; // Remote stream object
 
 let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    document.getElementById('user-1').srcObject = localStream
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    document.getElementById('user-1').srcObject = localStream;
 }
 
-init()
+init();
